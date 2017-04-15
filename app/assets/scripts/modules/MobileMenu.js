f39c31902ee1ddacb97097cd37263fc4d2927c1d
Tuatoe <tuatoe.daynuah@gmail.com>
2017-04-14 22:50:05 -0500

    new changes

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -1,6 +1,28 @@
 class MobileMenu{
     constructor(){
-      alert('testing from mobile menu')
+        alert('testing from mobile menu');
+    }
+}
+export default MobileMenu;
+
+/*
+import $ from 'jquery';
+
+class MobileMenu{
+    constructor(){
+      this.menuIcon = $('.site-header__menu-icon');
+      this.menuContent = $('.site-header__menu-content');
+      this.events();
+    }
+
+    events(){
+      this.menuIcon.click(this.toggleTheMenu.bind('hello world'));
+    }
+
+    toggleTheMenu(){
+      alert(this);
+      this.menuContent.toggleClass('site-header__menu-content--is-visible');
     }
 }
 export.default MobileMenu;
+*/
--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,16 @@ module.exports = {
   entry: './app/assets/scripts/App.js',
   output: {
     path: './app/temp/scripts',
-    filename: 'App.js'
+    filename: 'App.js',
   },
-  module:{
-    loaders: [
-      {
-        loader: 'babel',
-        query: {
-          presets: ['es2015']
-        },
-        test: /\.js$/,
-        exclude: /node_modules/
-      }
-    ]
-  }
-}
+  module: {
+    loaders: [{
+      loader: 'babel',
+      query: {
+        presets: ['es2015'],
+      },
+      test: /\.js$/,
+      exclude: /node_modules/,
+    }],
+  },
+};
